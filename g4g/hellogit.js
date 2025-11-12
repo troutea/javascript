13af0a927a1759231e5a1ef0100ba3e40372d637
troutea <troutea@yahoo.co.uk>
2025-11-12 21:25:39 +0000

    add playing around scripts

---
+++ g4g/hellogit.js
@@ -0,0 +1,18 @@
+
+
+let hello = "hello world"
+console.log(hello)
+
+let val = parseFloat('2.3') + parseFloat('2.4')
+
+console.log(val)
+
+function gfg_Run() {
+
+console.log("2.3 + 2.4 = "
+        + (parseFloat('2.3') +
+            parseFloat('2.4')).toFixed(2));
+
+}
+
+gfg_Run()
